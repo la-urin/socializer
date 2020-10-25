e1fffc1f9d0b286126007f9c832507ada00645a8
Loris Keller <loris.keller@hsr.ch>
2020-10-25 18:14:52 +0100

    Added button listeners to adapter

--- app/src/main/java/com/example/socializer/adapters/MessageAdapter.kt
+++ app/src/main/java/com/example/socializer/adapters/MessageAdapter.kt
@@ -4,23 +4,45 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.socializer.R
 import com.example.socializer.models.Message
 
-class MessageAdapter (context: Context) : RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
+class MessageAdapter(
+    context: Context,
+    private val onItemEditClicked: (Message) -> Unit,
+    private val onItemDeleteClicked: (Message) -> Unit
+) :
+    RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
 
     private val inflater: LayoutInflater = LayoutInflater.from(context)
     private var messages = emptyList<Message>()
 
-    inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val messageItemView: TextView = itemView.findViewById(R.id.group_name)
+    inner class MessageViewHolder(
+        itemView: View,
+        onItemEditClicked: (Int) -> Unit,
+        onItemDeleteClicked: (Int) -> Unit
+    ) :
+        RecyclerView.ViewHolder(itemView) {
+        init {
+            val editButton = itemView.findViewById<ImageButton>(R.id.message_view_item_button_edit)
+            editButton.setOnClickListener { onItemEditClicked(adapterPosition) }
+            val deleteButton =
+                itemView.findViewById<ImageButton>(R.id.message_view_item_button_delete)
+            deleteButton.setOnClickListener { onItemDeleteClicked(adapterPosition) }
+        }
+
+        val messageItemView: TextView = itemView.findViewById(R.id.message_view_item_text)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        val itemView = inflater.inflate(R.layout.group_view_item, parent, false)
-        return MessageViewHolder(itemView)
+        val itemView = inflater.inflate(R.layout.message_view_item, parent, false)
+
+        return MessageViewHolder(itemView,
+            { onItemEditClicked(messages[it]) },
+            { onItemDeleteClicked(messages[it]) })
     }
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
