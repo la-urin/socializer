4c4f43f669118e0e1d903142d21ce1e5105e08fe
Loris Keller <loris.keller@hsr.ch>
2020-10-25 15:39:46 +0100

    Refactoring MatTabs

--- app/src/main/java/com/example/socializer/activities/GroupEditActivity.kt
+++ app/src/main/java/com/example/socializer/activities/GroupEditActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import com.example.socializer.R
@@ -16,31 +17,45 @@ import com.example.socializer.viewmodels.GroupViewModel
 import com.google.android.material.tabs.TabLayout
 
 class GroupEditActivity : AppCompatActivity() {
+    private val messagesEditFragment = MessagesEditFragment()
+    private val contactsEditFragment = ContactsEditFragment()
+    private val groupEditFragment = GroupEditFragment()
+
+    private var currentFragment: Fragment = groupEditFragment;
+    private val fragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_edit)
 
-        val fragmentManager = supportFragmentManager;
-
+        fragmentManager.beginTransaction()
+            .add(R.id.activity_group_edit_fragment, currentFragment)
+            .commit()
 
         val tabLayout = findViewById<TabLayout>(R.id.activity_group_edit_tabs)
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab?.position) {
-                    1 -> fragmentManager.beginTransaction().replace(R.id.activity_group_edit_fragment, MessagesEditFragment()).commit()
-                    2 -> fragmentManager.beginTransaction().replace(R.id.activity_group_edit_fragment, ContactsEditFragment()).commit()
-                    else -> fragmentManager.beginTransaction().replace(R.id.activity_group_edit_fragment, GroupEditFragment()).commit()
-                }
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                // Handle tab reselect
+                setActiveView(tab)
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
-                // Handle tab unselect
+                fragmentManager.beginTransaction().remove(currentFragment).commit()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                setActiveView(tab)
             }
         })
+    }
+
+    private fun setActiveView(tab: TabLayout.Tab?) {
+        currentFragment = when (tab?.position) {
+            1 -> messagesEditFragment
+            2 -> contactsEditFragment
+            else -> groupEditFragment
+        }
+
+        fragmentManager.beginTransaction()
+            .add(R.id.activity_group_edit_fragment, currentFragment).commit()
     }
 }
