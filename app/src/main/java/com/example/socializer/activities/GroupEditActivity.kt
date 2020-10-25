d600ce41a6029fb5a4ac3cddd0cd8493f717f90b
Loris Keller <loris.keller@hsr.ch>
2020-10-25 16:19:43 +0100

    Readded save function for groups

--- app/src/main/java/com/example/socializer/activities/GroupEditActivity.kt
+++ app/src/main/java/com/example/socializer/activities/GroupEditActivity.kt
@@ -1,19 +1,12 @@
 package com.example.socializer.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.ViewModelProvider
 import com.example.socializer.R
 import com.example.socializer.fragments.ContactsEditFragment
 import com.example.socializer.fragments.GroupEditFragment
 import com.example.socializer.fragments.MessagesEditFragment
-import com.example.socializer.viewmodels.GroupViewModel
 import com.google.android.material.tabs.TabLayout
 
 class GroupEditActivity : AppCompatActivity() {
@@ -27,6 +20,13 @@ class GroupEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_edit)
+
+        val groupId = intent.getIntExtra("GroupId", 0)
+        val bundle = Bundle()
+        bundle.putInt("GroupId", groupId)
+        messagesEditFragment.arguments = bundle
+        contactsEditFragment.arguments = bundle
+        groupEditFragment.arguments = bundle
 
         fragmentManager.beginTransaction()
             .add(R.id.activity_group_edit_fragment, currentFragment)
--- app/src/main/java/com/example/socializer/fragments/GroupEditFragment.kt
+++ app/src/main/java/com/example/socializer/fragments/GroupEditFragment.kt
@@ -5,56 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.socializer.R
+import com.example.socializer.viewmodels.GroupViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM_GROUP_ID = "GroupId"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GroupEditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GroupEditFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var groupId: Int? = null
+    private lateinit var groupViewModel: GroupViewModel
+    private lateinit var nameEditText: EditText
+    private lateinit var saveButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            groupId = it.getInt(ARG_PARAM_GROUP_ID)
         }
+        
+        groupViewModel = ViewModelProvider(this).get(GroupViewModel::class.java)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.group_edit_fragment, container, false)
-    }
+        val group = groupViewModel.getById(groupId!!)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GroupEditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GroupEditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val fragment = inflater.inflate(R.layout.group_edit_fragment, container, false)
+        nameEditText = fragment.findViewById(R.id.group_edit_fragment_edit_name)
+        nameEditText.setText(group.name)
+
+        saveButton = fragment.findViewById(R.id.group_edit_fragment_button_save)
+        saveButton.setOnClickListener {
+            group.name = nameEditText.text.toString()
+            groupViewModel.update(group)
+            Toast.makeText(context, R.string.saved_successfully, Toast.LENGTH_SHORT).show()
+        }
+
+        return fragment
     }
 }
