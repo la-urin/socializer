753532c186e2cf11c43c95d049e23d44a82e65b2
laurin <27428798+la-urin@users.noreply.github.com>
2020-10-28 11:00:47 +0100

    update title main activity

--- app/src/main/java/com/example/socializer/activities/MainActivity.kt
+++ app/src/main/java/com/example/socializer/activities/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "${getString(R.string.app_name)} - ${getString(R.string.groups)}"
+        title = getString(R.string.app_name)
 
         setupGroupRecyclerView()
         setupAddGroupDialog()
